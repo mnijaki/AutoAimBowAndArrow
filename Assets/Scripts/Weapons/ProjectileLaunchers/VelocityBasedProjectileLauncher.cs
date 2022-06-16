74a6e939347088412826a7f45933c85a760a25b9
mnijaki <mnijaki@gmail.com>
2022-06-16 23:21:03 +0200

    * Refactor

--- Assets/Scripts/Weapons/ProjectileLaunchers/VelocityBasedProjectileLauncher.cs
+++ Assets/Scripts/Weapons/ProjectileLaunchers/VelocityBasedProjectileLauncher.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class VelocityBasedProjectileLauncher : MonoBehaviour
@@ -19,8 +20,22 @@ public class VelocityBasedProjectileLauncher : MonoBehaviour
 		_arrowPositionPredicter = GetComponent<ArrowPositionPredicter>();
 	}
 	
+	Vector3 tmp0 ;
+	Vector3 tmp1 ;
+	Vector3 tmp2 ;
+	Vector3 tmp3 ;
+
+	private void OnDrawGizmos()
+	{
+		Debug.DrawRay(tmp0, tmp1, Color.red);
+		Debug.DrawRay(tmp0, tmp2, Color.magenta);
+		Debug.DrawRay(tmp0, tmp3, Color.green);
+	}
+
 	public void Shoot(VelocityBasedWeaponType velocityBasedWeaponType, Vector3 firingPointPos, Vector3 targetPos)
 	{
+		Shoot2(velocityBasedWeaponType, firingPointPos, targetPos);
+		return;
 		// Oblique Throw - Trajectory (polish version)
 		// https://www.youtube.com/watch?v=jRfwUncRTyE
 		// Oblique throw range/distance equation:
@@ -36,17 +51,48 @@ public class VelocityBasedProjectileLauncher : MonoBehaviour
 		// we divide by 2
 		// angle  = [Math.Asin((dist * gravity) / (Vo*Vo)) * Mathf.Rad2Deg] / 2;
 
-		Vector3 directionToTarget = (targetPos - firingPointPos).normalized;
-		// rozlozyc powyzsze na dwie wysokosci (jedna gdy gracz i przeciwnik sa na tym samym poziomie, druga gdy nie sa)
+		var flattenTargetPos = new Vector3(targetPos.x, 0.0F, targetPos.z);
+		var flattenTargetPos2 = new Vector3(0, targetPos.y, 0.0F);
+		var flattenFiringPos = new Vector3(firingPointPos.x, 0.0F, firingPointPos.z);
+		var flattenFiringPos2 = new Vector3(0.0F, firingPointPos.y, 0.0F);
+		
+		float atanb = Mathf.Atan2((targetPos - firingPointPos).normalized.y, (targetPos - firingPointPos).normalized.z) * Mathf.Rad2Deg;
+		
 		float initialVelocity = velocityBasedWeaponType.InitialVelocity;
 		float distance = Vector3.Distance(targetPos,firingPointPos);
-		var sinAngle = (distance * _gravity.y) / (initialVelocity * initialVelocity);
-		float angle = (Mathf.Asin(sinAngle) * Mathf.Rad2Deg) / 2;
+		//float distance = Vector3.Distance(targetPos,firingPointPos) - Vector3.Distance(flattenTargetPos2,flattenFiringPos2);
+		//float distance = Vector3.Distance(flattenTargetPos,flattenFiringPos);
+		var sin2Angle = ((distance * _gravity.y) / (initialVelocity * initialVelocity));
+		float angle = Mathf.Asin( sin2Angle)  / 2 * Mathf.Rad2Deg;
 		// -90 and clamp to 90 because of lefthanded coordinate system?
 
-		//Vector3 velocityWithAngle = Quaternion.AngleAxis(-angle, Vector3.right) * (directionToTarget * initialVelocity);
-		Vector3 velocityWithAngle = Quaternion.AngleAxis(-angle, Vector3.right) * Vector3.up * initialVelocity;
+		
+		Vector3 directionToTarget = (targetPos - firingPointPos).normalized;
+		//Vector3 directionToTarget = (flattenTargetPos - flattenFiringPos).normalized;
+
+		var c = new Vector3(0.0F, (flattenTargetPos2 - flattenFiringPos2).y, 0.0F);
+		var cdist = Vector3.Distance(flattenTargetPos2, flattenFiringPos2);
+		var csinAngle = (cdist * _gravity.y) / (initialVelocity * initialVelocity);
+		float cangle = (Mathf.Asin(csinAngle) * Mathf.Rad2Deg) / 2;
+		cangle = Mathf.Atan2((targetPos - firingPointPos).normalized.y, (targetPos - firingPointPos).normalized.z) * Mathf.Rad2Deg -90;
+		//cangle = -cangle;
+		//cangle = 45 - cangle;
+		// cangle = 180 - cangle;
+		
+		Debug.Log("cangle = "+cangle);
+		tmp0 = firingPointPos;
+		tmp1 = Quaternion.AngleAxis(angle, Vector3.right) * (Vector3.forward * initialVelocity); // red
+		tmp2 = Quaternion.AngleAxis(cangle, Vector3.right) * (Vector3.forward * initialVelocity);; // magenta
+		tmp3 = Quaternion.AngleAxis(angle+cangle, Vector3.right) * (Vector3.forward * initialVelocity); // green
+
+		//var sum = Mathf.Clamp(angle + cangle, -45, 45);
+		//var sum = Mathf.Clamp(angle, -45, 45);
+		
+		Vector3 velocityWithAngle = Quaternion.AngleAxis(angle, Vector3.right) * (Vector3.forward * initialVelocity);
+		//Vector3 velocityWithAngle = Quaternion.AngleAxis(angle+cangle, Vector3.right) * (Vector3.forward * initialVelocity);
+		//velocityWithAngle = Quaternion.AngleAxis(cangle, Vector3.right) * velocityWithAngle;
 		velocityWithAngle = Quaternion.LookRotation(directionToTarget) * velocityWithAngle;
+		//velocityWithAngle = Quaternion.LookRotation((new Vector3(directionToTarget.x, angle, directionToTarget.z)).normalized) * velocityWithAngle;
 		
 		Debug.Log("velocity = "+(directionToTarget * initialVelocity));
 		Debug.Log("angle = "+angle);
@@ -60,43 +106,23 @@ public class VelocityBasedProjectileLauncher : MonoBehaviour
 	
 	public void Shoot2(VelocityBasedWeaponType velocityBasedWeaponType, Vector3 firingPointPos, Vector3 targetPos)
 	{
-		// Oblique Throw - Trajectory (polish version)
-		// https://www.youtube.com/watch?v=jRfwUncRTyE
-		// Oblique throw range/distance equation:
-		// dist = [Vo*Vo * sin(2*Angle)] / gravity
-		// we multiply by gravity
-		// dist * gravity = Vo*Vo * sin(2*Angle)
-		// we divide by (Vo*Vo)
-		// (dist * gravity) / (Vo*Vo) = sin(2*Angle)
-		// we mirror equation
-		// sin(2*Angle) = (dist * gravity) / (Vo*Vo)
-		// we use arcs sin to get angle form sin value. In Unity arcs sin is [Math.Asin() * Mathf.Rad2Deg]
-		// 2*Angle = Math.Asin((dist * gravity) / (Vo*Vo)) * Mathf/Rad2Deg
-		// we divide by 2
-		// angle  = [Math.Asin((dist * gravity) / (Vo*Vo)) * Mathf.Rad2Deg] / 2;
-
-
 		float initialVelocity = velocityBasedWeaponType.InitialVelocity;
 		float distance = Vector3.Distance(targetPos,firingPointPos);
-		var sinAngle = (distance * _gravity.y) / (initialVelocity * initialVelocity);
-		float angle = (Mathf.Asin(sinAngle) * Mathf.Rad2Deg) / 2;
+		var sin2Angle = ((distance * _gravity.y) / (initialVelocity * initialVelocity));
+		if(sin2Angle is < -1 or > 1)
+		{
+			Debug.Log("No shooting arc. Target too far. Exiting...");
+			return;
+		}
+		
+		float angle = Mathf.Asin(sin2Angle)  / 2 * Mathf.Rad2Deg;
 
 		Vector3 directionToTarget = (targetPos - firingPointPos).normalized;
-		Vector3 velocityWithAngle = directionToTarget * initialVelocity;
-		//velocityWithAngle = Quaternion.AngleAxis(angle, Vector3.right) * velocityWithAngle;
-		
-		// angle 45? too big too low?
-		velocityWithAngle = new Vector3(velocityWithAngle.x, velocityWithAngle.y-angle/2, velocityWithAngle.z);
-		
-		Debug.Log("targetPos = "+targetPos);
-		Debug.Log("firingPointPos = "+firingPointPos);
-		Debug.Log("distance = "+distance);
-		Debug.Log("angle = "+angle);
-		Debug.Log("directionToTarget = "+directionToTarget);
-		Debug.Log("velocityWithAngle = "+velocityWithAngle);
 
+		Vector3 velocityWithAngle = Quaternion.AngleAxis(angle, Vector3.right) * (Vector3.forward * initialVelocity);
+		velocityWithAngle = Quaternion.LookRotation(directionToTarget) * velocityWithAngle;
+		
 		float timeToReachTarget = Mathf.Abs(2 * (velocityWithAngle.y / _gravity.y));
-		//float timeToReachTarget =  Mathf.Abs(2 * ((velocityWithAngle.y * Mathf.Sin(angle)) / _gravity.y));
 		
 		ArrowLaunchData arrowLaunchData = new(firingPointPos, velocityWithAngle, timeToReachTarget , _gravity);
 		LaunchProjectile(arrowLaunchData);
